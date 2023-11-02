4af30e75765d1c8f85351c21e529f5a8454feac1
Christopher Magnus <minisungam@gmail.com>
2023-11-02 03:11:01 -0400

    hopefully dont have to touch anything now

---
+++ functions/getSusCD/getSusCD.js
@@ -0,0 +1,48 @@
+const fs = require('fs').promises; // Use the promises version of fs
+const ical = require('node-ical');
+
+const icsFile = './grizzlies.ics';
+
+const handler = async (event) => {
+  try {
+    const daysLeft = await updateSuspensionCountdown();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: daysLeft }),
+    };
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() };
+  }
+};
+
+module.exports = { handler };
+
+// Update the suspension countdown
+async function updateSuspensionCountdown() {
+  try {
+    const data = await fs.readFile(icsFile, 'utf8');
+    // Parse the .ICS data
+    const parsedData = ical.parseICS(data);
+
+    const gamesLeft = calculateRemainingGames(parsedData);
+    if (gamesLeft < 0) {
+      gamesLeft = 0;
+    }
+
+    console.log(`Updated suspension countdown: ${gamesLeft} games left.`);
+    return gamesLeft;
+  } catch (err) {
+    console.error('Error reading .ICS file:', err);
+    throw err; // Rethrow the error to be caught in the calling function
+  }
+}
+
+// Calculate the remaining games until suspension is over
+function calculateRemainingGames(parsedData) {
+  const today = new Date();
+  const playedGames = Object.values(parsedData).filter((event) => event.start <= today).length;
+
+  // Subtract the number of played games from the total suspension games
+  const totalSuspensionGames = 25;
+  return totalSuspensionGames - playedGames;
+}
